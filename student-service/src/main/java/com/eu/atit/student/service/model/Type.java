59cc28db8d0638b41638273caf210cfcd2a3503c
asutalo <a.sutalo@hotmail.com>
2022-12-05 08:30:06 +0000

    updated Type model to be compatible with generic BQ service

--- student-service/src/main/java/com/eu/atit/student/service/model/Type.java
+++ student-service/src/main/java/com/eu/atit/student/service/model/Type.java
@@ -1,4 +1,29 @@
 package com.eu.atit.student.service.model;
 
-public record Type(int id, String name) {
+import com.eu.atit.mysql.service.annotations.MySqlField;
+import com.mysql.cj.MysqlType;
+
+public final class Type {
+    @MySqlField(type = MysqlType.INT, primary = true)
+    private Integer id;
+    @MySqlField(type = MysqlType.VARCHAR)
+    private String name;
+
+    public Type(String name) {
+        this.name = name;
+    }
+
+    private Type(){}
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Integer id() {
+        return id;
+    }
 }
