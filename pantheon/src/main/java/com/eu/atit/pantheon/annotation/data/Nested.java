9e1ece22fccd9527664118afa970407a8e7290d8
asutalo <a.sutalo@hotmail.com>
2023-01-18 15:58:50 +0000

    cleanup... todo: inserting with nested objects (just insert IDs of nested objects into correct table, not insert actual nested object to DB) unification of terms (coulmName vs fieldName) refactor tests readme

--- pantheon/src/main/java/com/eu/atit/pantheon/annotation/data/Nested.java
+++ pantheon/src/main/java/com/eu/atit/pantheon/annotation/data/Nested.java
@@ -10,13 +10,34 @@ import java.lang.annotation.Target;
 public @interface Nested {
 
     boolean outward() default false;
+
     boolean inward() default false;
+    /**
+     * in an outward relationship the link is the name of the column in the parent that points to the main ID column of the child
+     * in an inward relationship the link is the name of the column in the child that points to the main ID column of the parent
+     * */
 
     String link() default "";
 
     boolean eager() default false;
 
+    /**
+     * used only for nested Lists, i.e. n:n relationship
+     * the name of the connecting table, if not specified then it is inferred from the parent class_listed class
+     * */
     String connection() default "";
+
+    /**
+     * used only for nested Lists, i.e. n:n relationship
+     * represents the ID of the parent in the connecting table
+     * if not specified then it is inferred as name of the parent class_ID column
+     * */
     String from() default "";
+
+    /**
+     * used only for nested Lists, i.e. n:n relationship
+     * represents the ID of the child in the connecting table
+     * if not specified then it is inferred as name of the child class_ID column
+     * */
     String to() default "";
 }
